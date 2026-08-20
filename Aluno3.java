32b81b6ea7750fac69934986c399520c6f966fa8
antonioaugustofrota02-cmyk <antonio.augusto.frota02@aluno.ifce.edu.br>
2026-08-19 23:23:40 -0300

    Projeto1

---
+++ Aluno3.java
@@ -0,0 +1,19 @@
+public class Aluno3 {
+	
+	private String nome;
+	private float nota;
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	public String getNome(){
+		return this.nome;
+	}
+	public void setNota(float nota){
+		this.nota = nota;
+	}
+	public float getNota(){
+		return this.nota;
+	}
+	
+}
